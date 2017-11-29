beb58233a7ed70178affa4552d34a51890497655
Sergey Petrov <desoulate777@gmail.com>
2017-11-29 10:50:10 +0300

    Fix UserCreationForm

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,11 @@
 from django.contrib.auth.forms import UserCreationForm as BaseUserCreationForm
 
+from .models import User
+
 
 class UserCreationForm(BaseUserCreationForm):
 
     use_required_attribute = False
+
+    class Meta(BaseUserCreationForm.Meta):
+        model = User
